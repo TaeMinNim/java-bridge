6f021bf94485bfece053865b184da111b1cbd100
TaeMin <xoals3094@naver.com>
2022-12-13 18:43:13 +0900

    Fix : 입력부 메시지 출력 추가

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -11,6 +11,7 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public Size readBridgeSize() {
+        System.out.println("다리의 길이를 입력해주세요.");
         String string = Console.readLine();
         int number;
         try {
@@ -26,6 +27,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public Move readMoving() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String string = Console.readLine();
 
         return new Move(string);
@@ -35,6 +37,7 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public Command readGameCommand() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String string = Console.readLine();
 
         return new Command(string);

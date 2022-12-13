ecb235e8b08c6a60b944ed2cded22252a47d4569
TaeMin <xoals3094@naver.com>
2022-12-13 18:25:51 +0900

    Feat : 다리 길이를 입력받는 기능 구현

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,8 +10,16 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public Size readBridgeSize() {
+        String string = Console.readLine();
+        int number;
+        try {
+            number = Integer.parseInt(string);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException("올바른 값을 입력해주세요");
+        }
+
+        return new Size(number);
     }
 
     /**
---
+++ src/main/java/bridge/Size.java
@@ -0,0 +1,19 @@
+package bridge;
+
+public class Size {
+    private int size;
+
+    public Size(int size){
+        validData(size);
+    }
+
+    private void validData(int size){
+        if(!(size >= 3 && size <= 20)){
+            throw new IllegalArgumentException("3부터 20 사이의 숫자만 입력 가능합니다");
+        }
+    }
+
+    public int getSize(){
+        return size;
+    }
+}

94b25dbd91f681b6f23c8e887113bbee126b1f19
TaeMin <xoals3094@naver.com>
2022-12-13 20:19:37 +0900

    Feat(BridgeGame) : 시도 횟수 카운트 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -14,6 +14,7 @@ public class BridgeGame {
     private final Bridge bridge;
     private Player player;
     private String moveResult;
+    private int tryCount = 1;
 
     public BridgeGame(List<String> bridge){
         this.bridge = new Bridge(bridge);
@@ -60,6 +61,7 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        tryCount++;
         initGame();
     }
 
@@ -73,5 +75,9 @@ public class BridgeGame {
 
     public List<String> getPlayer(){
         return player.getChoiceLog();
+    }
+
+    public int getTryCount(){
+        return tryCount;
     }
 }
